313e711af6229fbd6a081cbb8c8351c3ef3b5f16
Oluwatobiloba Ogunye <ogunye4nao@gmail.com>
2019-04-07 07:47:14 +0100

    update

--- helloweb.go
+++ helloweb.go
@@ -1,11 +1,34 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
+
+// User is a representation of a user
+type User struct {
+	FirstName string `json:"firstname"`
+	LastName  string `json:"lastname"`
+	Age       int    `json:"age"`
+}
+
+func decode(w http.ResponseWriter, r *http.Request) {
+	var user User
+	json.NewDecoder(r.Body).Decode(&user)
+	fmt.Fprintf(w, "%s %s is %d years old!", user.FirstName, user.LastName, user.Age)
+}
+
+func encode(w http.ResponseWriter, r *http.Request) {
+	user := User{
+		FirstName: "John",
+		LastName:  "Doe",
+		Age:       25,
+	}
+	json.NewEncoder(w).Encode(&user)
+}
 
 func greet(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello World! %s\n", r.URL.Path)
@@ -23,6 +46,8 @@ func getBook(w http.ResponseWriter, r *http.Request) {
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", greet)
+	r.HandleFunc("/encode", encode)
+	r.HandleFunc("/decode", decode)
 	r.HandleFunc("/books/{title}/page/{page}", getBook)
 
 	http.ListenAndServe(":8080", r)
